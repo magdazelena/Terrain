6fca3d51181dafa30508db51c21481067fb9d398
magdazelena <magda.zelezik@gmail.com>
2020-05-15 11:25:34 +0200

    Canvas setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useRef } from 'react';
+import THREE from './3d/three';
+import SimplexNoise from 'simplex-noise';
 function App() {
+  let canvasRef = useRef();
+  let cols, rows, scene, renderer, camera;
+  let scale = 20;
+  let width = 2000;
+  let height = 2000;
+  let setup = ()=>{
+        const canvas = canvasRef.current;
+        const backgroundColor = 0x1d1c3a;
+        cols = width/scale;
+        rows = height/scale;
+        //scene
+        scene = new THREE.Scene();
+        scene.background = new THREE.Color(backgroundColor);
+        //renderer
+        renderer = new THREE.WebGLRenderer({ canvas, antialias: true, alpha: true });
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.setClearColor(0x000000, 0);
+        renderer.shadowMap.enabled = true;
+        ///this.renderer.setPixelRatio(window.devicePixelRatio);
+        document.body.appendChild(renderer.domElement);
+        //camera
+        camera = new THREE.PerspectiveCamera(
+            50,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            1000
+          );
+          camera.position.z = 30 
+          camera.position.x = 0;
+          camera.position.y = -3;
+          //lights
+          let hemiLight = new THREE.HemisphereLight(0xffffff,  0.91);
+            hemiLight.position.set(0, 50, 0);
+            // Add hemisphere light to scene
+            scene.add(hemiLight);
+
+      
+  }
+  let draw = () =>{
+    requestAnimationFrame(draw);
+          renderer.render(scene, camera);
+       
+
+  }
+  setup();
+  draw();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <canvas ref={canvasRef}></canvas>
     </div>
   );
 }
